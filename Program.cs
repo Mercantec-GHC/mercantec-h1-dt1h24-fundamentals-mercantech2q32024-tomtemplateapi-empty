83c35c48bcdf420fd1999a69a746877c96bcb583
wivelsted <vika.forever.21@gmail.com>
2024-10-18 07:58:20 +0200

    challanges

--- Program.cs
+++ Program.cs
@@ -1,288 +1,446 @@
-﻿//Microsoft Learn excercises
-//Excercise 1. Write your first C# code
-//1.1
-using System.Reflection.Metadata;
+﻿using System;
+////Microsoft Learn excercises
 
-Console.WriteLine("Hello World!");
-//Console.WriteLine("Hello World");
-Console.WriteLine();
-Console.WriteLine("----------------------------");
-//1.2
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.Write("You wrote your first line of code!");
+//Random dice = new();
+//int roll1 = dice.Next();
+//int roll2 = dice.Next(101);
+//int roll3 = dice.Next(50, 101);
 
-Console.WriteLine();
-Console.WriteLine("----------------------------");
+//Console.WriteLine($"First roll:  {roll1}");
+//Console.WriteLine($"Second roll: {roll2}");
+//Console.WriteLine($"Third roll: {roll3}");
 
-Console.WriteLine("Congratulations!");
-Console.Write("You wrote your first code.");
+//int firstValue = 500;
+//int secondValue = 600;
+//int largerValue = Math.Max(firstValue, secondValue);
 
-Console.WriteLine();
-Console.WriteLine("----------------------------");
-//1.4.1
-Console.WriteLine("This is the first line.");
-Console.WriteLine("This is the second line.");
-//1.4.2
-Console.Write("This is the first line.\n");
-Console.WriteLine("This is the second line.");
-//1.4.3
-Console.WriteLine("This is the first line.");
 
-Console.Write("This is ");
-Console.Write("the second ");
-Console.Write("line.");
+//Console.WriteLine(largerValue);
 
-Console.WriteLine();
-Console.WriteLine("----------------------------");
-//Excercise 2. Store and retrieve data using literal and variable values in C# 
-//2.2
-Console.WriteLine('b');//char
-Console.WriteLine(8); //int
-Console.WriteLine(0.13F);//float
-Console.WriteLine(55.864);//double
-Console.WriteLine(15.12345678901236540m);//decimal
-Console.WriteLine(true);
-Console.WriteLine(false);
+//Add decision logic to your code using `if`, `else`, and `else if` statements in C#
 
-Console.WriteLine("----------------------------");
-Console.WriteLine();
-//2.4.1
-string firstName;
-firstName = "Robert";
-Console.WriteLine(firstName);
-firstName = "Laura";
-Console.WriteLine(firstName);
-firstName = "Lucky";
-Console.WriteLine(firstName);
-firstName = "Den";
-Console.WriteLine(firstName);
+//Random dice = new Random();
 
-Console.WriteLine("----------------------------");
-Console.WriteLine();
-//2.4.2
-string firstNameInitializing = "Robert";
-Console.WriteLine(firstNameInitializing);
+//int roll1 = dice.Next(1, 7);
+//int roll2 = dice.Next(1, 7);
+//int roll3 = dice.Next(1, 7);
 
-Console.WriteLine("----------------------------");
-Console.WriteLine();
-//2.6
-string firstName2 = "Bob";
-int numberOfMessages = 3;
-double temperatureCelcius = 34.4;
-Console.WriteLine("Hello, " + firstName2 + "! You have " + numberOfMessages + " messages in your inbox. The temperature is " + temperatureCelcius + " celsius.");
+//int total = roll1 + roll2 + roll3;
 
-Console.WriteLine("----------------------------");
-Console.WriteLine();
-//Excercise 3. Combine strings using character escape sequences
-//3.2
-Console.WriteLine("Hello\nWorld!");
-Console.WriteLine("Hello\tWorld!");
-Console.WriteLine("Hello \"World\"!");
-Console.WriteLine("c:\\source\\repos");
-Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
-Console.WriteLine("Invoice: 1021\t\tComplete!");
-Console.WriteLine("Invoice: 1022\t\tComplete!");
-Console.WriteLine("\nOutput Directory:\t");
-Console.WriteLine(@"c:\invoices");
+//Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-Console.WriteLine("----------------------------");
-Console.WriteLine();
+//if (total < 14)
+using System.Runtime.CompilerServices;
 
-//3.3
-string greeting = "Hello ";
-string message = greeting + " " + firstName2 + "!";
-Console.WriteLine(message);
-Console.WriteLine(greeting + " " + firstName2 + "!");
+{
+	//Console.WriteLine("You win!");
+}
+//else if (total > 15)
+{
+	//Console.WriteLine("You lost :( ");
+}
 
-Console.WriteLine("----------------------------");
-Console.WriteLine();
+//if (roll1 == roll2 || roll2 == roll3 || roll3 == roll1)
+{
+	//Console.WriteLine("Bonus!");
+	//total += 2;
+	//Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+}
 
-//3.4
-Console.WriteLine($"{greeting} {firstName2}!");
+//Excercise Create nested decision logic with if, else if, and else
 
-string message2 = $"Hello {firstName2}!";
-Console.WriteLine(message2);
+Random dice = new Random();
 
-int updateVersion = 11;
-string updateText = "Update to Windows";
-string updateMessage = $"{updateText} {updateVersion}";
-Console.WriteLine(updateMessage);
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
 
-Console.WriteLine($"{updateText} {updateVersion}!");
+int total = roll1 + roll2 + roll3;
 
-string someProjectName = "Some_Project";
-Console.WriteLine($@"C:\Output\{someProjectName}\Data");
+//Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-Console.WriteLine("----------------------------");
-Console.WriteLine();
-//3.5
-string projectName = "ACME";
-string englishMessage = "View English output: ";
-Console.WriteLine(englishMessage);
-Console.WriteLine($@"c:\Exercise\{projectName}\data.txt");
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+	if ((roll1 == roll2) && (roll2 == roll3))
+	{
+		//Console.WriteLine("You rolled triples! +6 bonus to total!");
+		total += 6;
+	}
+	else
+	{
+		//Console.WriteLine("You rolled doubles! +2 bonus to total!");
+		total += 2;
+	}
 
-string russianLanguage = "\u0440\u0443 \u02c9 \u0420\u0423";
-string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-Console.WriteLine(russianMessage);
-Console.WriteLine($@"c:\Exercise\{projectName}\{russianLanguage}\data.txt");
+}
 
-Console.WriteLine("----------------------------");
-Console.WriteLine();
-//Excercise 4. Perform addition with implicit data conversion
-//4.2
-int firstNumber = 12;
-int secondNumber = 7;
-Console.WriteLine(firstNumber + secondNumber);
+if (total >= 15)
+{
+	//Console.WriteLine("You win!");
+}
+else
+{
+	//Console.WriteLine("Sorry, you lose.");
+}
 
-int widgetsSold = 7;
-Console.WriteLine(firstName2 + " sold " + (widgetsSold + 22) + " widgets.");
+string[] names = { "Rowena", "Robin", "Bao" };
+foreach (string name in names)
+{
+	//Console.WriteLine(name);
+}
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+foreach (int item in inventory)
+{
+	//Console.WriteLine(item);
+	sum += item;
+}
+//Console.WriteLine($"We have {sum} items in inventory.");
 
-Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
-Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+string[] list = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+foreach (string item in list)
+{
+	if (item.StartsWith("C"))
+	{
+		//Console.WriteLine(item);
+	}
 
-int value1 = 3 + 4 * 5;
-int value2 = (3 + 4) * 5;
-Console.WriteLine(value1);
-Console.WriteLine(value2);
+}
 
-//4.4
-//int value = 7;
 
-//value = value + 1;
-//Console.WriteLine("First increment: " + value);
+//------------------------------------------------------------------------------------------------------------
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
 
-//value += 1;
-//Console.WriteLine("Second increment: " + value);
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+int x = 0;
 
-//value++;
-//Console.WriteLine("Third increment: " + value);
+foreach (char i in message)
+{
+	if (i == 'o')
+	{
+		x++;
+	}
+}
 
-//value = value - 1;
-//Console.WriteLine("First decrement: " + value);
+string newMessage = new String(message);
 
-//value -= 1;
-//Console.WriteLine("Second decrement: " + value);
+//Console.WriteLine(newMessage);
+//Console.WriteLine($"'o' appears {x} times.");
 
-//value--;
-//Console.WriteLine("Third decrement: " + value);
-
-int value = 8;
-value++;
-Console.WriteLine("First: " + value);
-Console.WriteLine($"Second: {value++}");
-Console.WriteLine("Third: " + value);
-Console.WriteLine("Fourth: " + (++value));
-
-//4.5
-int fahrenheit = 94;
-double devided = 5.0 / 9.0;
-double celsius = (fahrenheit - 32) * devided;
-Console.WriteLine($"The temperature is {celsius} Celsius");
-
-Console.WriteLine("----------------------------");
-Console.WriteLine();
-
-//Excercise 5. Guided project - Calculate and print student grades
-
+//------------------------------------------------------------------------------------------------
 int currentAssignments = 5;
 
-int sophia1 = 93;
-int sophia2 = 87;
-int sophia3 = 98;
-int sophia4 = 95;
-int sophia5 = 100;
-int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-decimal sophiaScore = (decimal)sophiaSum / currentAssignments;
+int[] jensScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] jesperScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] johnScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] joseScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
 
-int nicolas1 = 80;
-int nicolas2 = 83;
-int nicolas3 = 82;
-int nicolas4 = 88;
-int nicolas5 = 85;
-int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
-decimal nicolasScore = (decimal)nicolasSum / currentAssignments;
+// Student names
+string[] students = new string[] { "Jens", "Jesper", "John", "Jose", "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
 
-int zahirah1 = 84;
-int zahirah2 = 96;
-int zahirah3 = 73;
-int zahirah4 = 85;
-int zahirah5 = 79;
-int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
-decimal zahirahScore = (decimal)zahirahSum / currentAssignments;
+int[] studentScores = new int[10];
 
-int jeong1 = 90;
-int jeong2 = 92;
-int jeong3 = 98;
-int jeong4 = 100;
-int jeong5 = 97;
-int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
-decimal jeongScore = (decimal)jeongSum / currentAssignments;
+//Console.WriteLine("Student\t\tGrade\n");
+foreach (string student in students)
+{
+	string currentStudent = student;
+	if (currentStudent == "Jens")
+		studentScores = jensScores;
+	else if (currentStudent == "Jesper")
+		studentScores = jesperScores;
+	else if (currentStudent == "John")
+		studentScores = johnScores;
+	else if (currentStudent == "Jose")
+		studentScores = joseScores;
+	else if(currentStudent == "Sophia")
+		studentScores = sophiaScores;
+	else if (currentStudent == "Andrew")
+		studentScores = andrewScores;
+	else if (currentStudent == "Emma")
+		studentScores = emmaScores;
+	else if (currentStudent == "Logan")
+		studentScores = loganScores;
+	else if (currentStudent == "Becky")
+		studentScores = beckyScores;
+	else if (currentStudent == "Chris")
+		studentScores = chrisScores;
+	else if (currentStudent == "Eric")
+		studentScores = ericScores;
+	else if (currentStudent == "Gregor")
+		studentScores = gregorScores;
+	else
+		continue;
 
-Console.WriteLine("Student Grade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
-Console.WriteLine("Nicolas:\t" + nicolasScore + "\tB");
-Console.WriteLine("Zahirah:\t" +  zahirahScore + "\tB");
-Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA");
 
-Console.WriteLine("----------------------------");
-Console.WriteLine();
-//Excercise 6. Guided project - Calculate final GPA
 
-string studentName = "Sophia Johnson";
+	int sumAssignmentScores = 0;
+	decimal currentStudentGrade = 0;
+	int extraScores = 0;
+	string exam = "";
 
-string course1Name = "English 101";
-string course2Name = "Algebra 101";
-string course3Name = "Biology 101";
-string course4Name = "Computer Science I";
-string course5Name = "Psychology 101";
+	foreach (int score in studentScores)
+	{
+		extraScores += 1;
 
-int course1Credit = 3;
-int course2Credit = 3;
-int course3Credit = 4;
-int course4Credit = 4;
-int course5Credit = 3;
+		if (extraScores <= currentAssignments)
+			sumAssignmentScores += score;
+		else
+			sumAssignmentScores += score / 10;
+	}
 
-int gradeA = 4;
-int gradeB = 3;
+	currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+	
+	if (currentStudentGrade <= 59)
+	{
+		exam = "F";
+	}
+	else if (currentStudentGrade >= 60 && currentStudentGrade <= 62)
+	{
+		exam = "D-";
+	}
+	else if (currentStudentGrade >= 63 && currentStudentGrade <= 66)
+	{
+		exam = "D";
+	}
+	else if (currentStudentGrade >= 67 && currentStudentGrade <= 69)
+	{
+		exam = "D+";
+	}
+	else if (currentStudentGrade >= 70 && currentStudentGrade <= 72)
+	{
+		exam = "C-";
+	}
+	else if (currentStudentGrade >= 73 && currentStudentGrade <= 76)
+	{
+		exam = "C";
+	}
+	else if (currentStudentGrade >= 77 && currentStudentGrade <= 79)
+	{
+		exam = "C+";
+	}
+	else if (currentStudentGrade >= 80 && currentStudentGrade <= 82)
+	{
+		exam = "B-";
+	}
+	else if (currentStudentGrade >= 83 && currentStudentGrade <= 86)
+	{
+		exam = "B";
+	}
+	else if (currentStudentGrade >= 87 && currentStudentGrade <= 89)
+	{
+		exam = "B+";
+	}
+	else if (currentStudentGrade >= 90 && currentStudentGrade <= 92)
+	{
+		exam = "A-";
+	}
+	else if (currentStudentGrade >= 93 && currentStudentGrade <= 96)
+	{
+		exam = "A";
+	}
+	else
+	{
+		exam = "A+";
+	}
 
-int course1Grade = gradeA;
-int course2Grade = gradeB;
-int course3Grade = gradeB;
-int course4Grade = gradeB;
-int course5Grade = gradeA;
+	//Console.WriteLine($"{currentStudent}:\t\t {currentStudentGrade}\t{exam}");
 
-int totalCreditHours = 0;
-totalCreditHours += course1Credit;
-totalCreditHours += course2Credit;
-totalCreditHours += course3Credit;
-totalCreditHours += course4Credit;
-totalCreditHours += course5Credit;
-totalCreditHours = course1Credit + course2Credit + course3Credit + course4Credit + course5Credit;
+}
+//Console.WriteLine("Press the Enter key to continue");
+//Console.ReadKey();
+//----------------------------------------------------------------------------------------------------
+//string value1 = "A";
+//string value2 = "a ";
+//Console.WriteLine(value1.ToLower() == value2.Trim());
+//Console.WriteLine("a" != "a");
+//Console.WriteLine("a" != "A");
+//Console.WriteLine(1 != 2);
 
-int totalGradePoints = 0;
-totalGradePoints += course1Grade * course1Credit;
-totalGradePoints += course2Grade * course2Credit;
-totalGradePoints += course3Grade * course3Credit;
-totalGradePoints += course4Grade * course4Credit;
-totalGradePoints += course5Grade * course5Credit;
+//string myValue = "a";
+//Console.WriteLine(myValue != "a");
 
-//Console.WriteLine($"{totalGradePoints} {totalCreditHours}");
+//string pangram = "The quick brown fox jumps over the lazy dog.";
+//Console.WriteLine(pangram.Contains("fox"));
+//Console.WriteLine(pangram.Contains("cow"));
 
-decimal gradePointAvarage = (decimal)totalGradePoints/totalCreditHours;
-int leadingDigit = (int)gradePointAvarage;
-int firstDigit = (int)(gradePointAvarage * 10) % 10;
+int saleAmount = 50;
+//int discount = saleAmount > 1000 ? 100 : 50;
+//Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50 )}");
 
-int secondDigit = (int)(gradePointAvarage * 100) % 10;
 
-Console.WriteLine($"Student: " + studentName + "\n");
-Console.WriteLine($"Course\t\t\t Grade\tCredit\tHours");
-Console.WriteLine($"{course1Name}\t\t {course1Grade}\t {course1Credit}");
-Console.WriteLine($"{course2Name}\t\t {course2Grade}\t {course2Credit}");
-Console.WriteLine($"{course3Name}\t\t {course3Grade}\t {course3Credit}");
-Console.WriteLine($"{course4Name}\t {course4Grade}\t {course4Credit}");
-Console.WriteLine($"{course5Name}\t\t {course5Grade}\t {course5Credit} \n");
-Console.WriteLine($"Final GPA:\t\t {leadingDigit}.{firstDigit}{secondDigit}");
+Random coin = new Random();
+//int side = coin.Next(0, 2);
 
-Console.WriteLine("----------------------------");
-Console.WriteLine();
+//Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
+
+//___________________________________________________________________
+//___________________________________________________________________
+
+//int[] times = { 800, 1200, 1600, 2000 };
+//int diff = 0;
+
+//Console.WriteLine("Enter current GMT");
+//int currentGMT = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Current Medicine Schedule:");
+//DisplayTimes();
+
+
+//Console.WriteLine("Enter new GMT");
+//int newGMT = Convert.ToInt32(Console.ReadLine());
+
+//if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+//{
+//	Console.WriteLine("Invalid GMT");
+//}
+//else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+//{
+//	diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+//	AdjustTimes();
+//}
+//else
+//{
+//	diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+//	AdjustTimes();
+//}
+
+//Console.WriteLine("New Medicine Schedule:");
+//DisplayTimes();
+
+//Console.WriteLine();
+
+//void DisplayTimes()
+//{
+//	/* Format and display medicine times */
+//	foreach (int val in times)
+//	{
+//		string time = val.ToString();
+//		int len = time.Length;
+
+//		if (len >= 3)
+//		{
+//			time = time.Insert(len - 2, ":");
+//		}
+//		else if (len == 2)
+//		{
+//			time = time.Insert(0, "0:");
+//		}
+//		else
+//		{
+//			time = time.Insert(0, "0:0");
+//		}
+
+//		Console.Write($"{time} ");
+//	}
+
+//	Console.WriteLine();
+//}
+//void AdjustTimes()
+//{
+//	/* Adjust the times by adding the difference, keeping the value within 24 hours */
+//	for (int i = 0; i < times.Length; i++)
+//	{
+//		times[i] = ((times[i] + diff)) % 2400;
+//	}
+//}
+
+
+
+//Console.ReadKey();
+
+//CountTo(5);
+
+//void CountTo(int max)
+//{
+//	for (int i = 0; i < max; i++)
+//	{
+//		Console.Write($"{i}, ");
+//	}
+//}
+
+//int[] schedule = { 800, 1200, 1600, 2000 };
+//DisplayAdjustedTimes(schedule, 6, -6);
+
+//void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+//{
+//	int diff = 0;
+//	if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+//	{
+//		Console.WriteLine("Invalid GMT.");
+//	}
+//	else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+//	{
+//		diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+//	}
+//	else
+//	{
+//		diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+//	}
+//}
+
+//string[] students1 = { "Jenna", "Ayesha", "Carlos", "Viktor" };
+
+//DisplayStudents(students1);
+//DisplayStudents(new string[] { "Robert", "Vanya" });
+
+//void DisplayStudents(string[] students1)
+//{
+//	foreach (string student in students1)
+//	{
+//		Console.Write($"{student}, ");
+//	}
+//	Console.WriteLine();
+//}
+
+string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+string[] rsvps = new string[10];
+int count = 0;
+
+RSVP("Rebecca", 1, "none", false);
+RSVP("Nadia", 2, "nuts", true);
+RSVP("Noor", 1, "none", true);
+RSVP("Jonte", 3, "gluten", false);
+RSVP("Linh", 2, "none", false);
+RSVP("Tony", 1, "Jackfruit", true);
+ShowRSVPs();
+void RSVP(string name, int partySize, string allergies, bool inviteOnly)
+{
+	if (inviteOnly)
+	{
+		bool found = false;
+		foreach (string guest in guestList)
+		{
+			if (guest.Equals(name))
+			{
+				found = true;
+				break;
+			}
+		}
+		if (!found)
+		{
+			Console.WriteLine($"Sorry, {name} is not on the guest list");
+			return;
+		}
+	}
+
+	rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+	count++;
+}
+
+void ShowRSVPs()
+{
+	Console.WriteLine("\nTotal RSVPs:");
+	for (int i = 0; i < count; i++)
+	{
+		Console.WriteLine(rsvps[i]);
+	}
+}
